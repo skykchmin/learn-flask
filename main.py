344e3c72fe76da18eaf7aaaa8fbaad3c313886d2
skykchmin <skykchmin@naver.com>
2021-12-20 23:55:40 +0900

    [Create] Create main.py Flask - 211220
    Flask app 호출

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    return f"<h1>Hello</h1>"
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port="8080");

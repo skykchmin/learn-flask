6b4e4366c516d5f48629454a87448ec608aeeff3
skykchmin <skykchmin@naver.com>
2021-12-22 23:53:49 +0900

    [add] How to routing - 211222
    routing 인자 처리, 타입 힌트 적용

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)   # Flask 객체 생성
 
-@app.route("/hello")
+def add_file(data):
+    return data + 5
+
+@app.route("/hello")    # 라우트 설정
 def hello():
     return f"<h1>Hello</h1>"
+
+@app.route('/profile/<username>')   #파라미터 인자
+def get_profile(username: str) -> str:
+    return "profile:" + username
+
+@app.route('/message/<int:message_id>') #파라미터 인자 받을때 타입 힌트를 적용가능
+def get_message(message_id: int) -> int:
+    return "message id: %d" % message_id
+
+@app.route("/first/<int:messageid>")
+def get_first(messageid):
+    data = add_file(messageid)
+    return "<h1>%d</h1>" % (data)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port="8080");
